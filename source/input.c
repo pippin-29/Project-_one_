e2a4200dfe76bb4ccbd0ddc47d4d94aa9ea50147
Daniel Haddington <haddingtondaniel5@gmail.com>
2024-10-20 17:33:48 +1030

    removed print statements

--- source/input.c
+++ source/input.c
@@ -23,16 +23,13 @@ none	input_(t_program *c)
 	int key = GetCharPressed();
 	if(isprint(key))
 	{
-		printf("Printable Character: %c\n", key);
+
 	}
 	if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
 	{
 		c->mouse_position = GetMousePosition();
-
-		printf(" mouse_x: %f  mouse_y: %f\n", c->mouse_position.x, c->mouse_position.y);
 		c->cursor_x = (int)((c->mouse_position.x - OFFSET_X) / BOX_WIDTH);
 		c->cursor_y = (int)((c->mouse_position.y - OFFSET_Y) / BOX_HEIGHT);
-		printf(" cursor_x: %d  cursor_y: %d\n", c->cursor_x, c->cursor_y);
 		if (c->cursor_x < 0) c->cursor_x = 0;
 		if (c->cursor_x > 127) c->cursor_x = 127;
 		if (c->cursor_y < 0) c->cursor_y = 0;
