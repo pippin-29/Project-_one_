c08c44b4308c4ef4fe97e22f3daf27223f534463
Daniel Haddington <haddingtondaniel5@gmail.com>
2024-10-20 17:31:06 +1030

    added code readability

--- source/main.c
+++ source/main.c
@@ -25,23 +25,8 @@ int main() {
     // Main loop
 
 	while (!WindowShouldClose())
-	{
-    	if ((IsKeyPressed(KEY_LEFT) || IsKeyPressedRepeat(KEY_LEFT))&& c->cursor_x > 0)
-			c->cursor_x -= 1;
-    	if ((IsKeyPressed(KEY_RIGHT) || IsKeyPressedRepeat(KEY_RIGHT)) && c->cursor_x < 127)
-			c->cursor_x += 1;  
-    	if ((IsKeyPressed(KEY_UP) || IsKeyPressedRepeat(KEY_UP)) && c->cursor_y > 0)
-    	    c->cursor_y -= 1;
-    	if ((IsKeyPressed(KEY_DOWN) || IsKeyPressedRepeat(KEY_DOWN)) && c->cursor_y < 31)
-    	    c->cursor_y += 1;
-
-		int key = GetCharPressed();
-		if(isprint(key))
-		{
-			printf("Printable Character: %c\n", key);
-		}
-			
-
+	{	
+		input_(c);
 		BeginDrawing();
 		draw__one_(c);
 		EndDrawing();
